2b93e2b93498b485ee33f44f359d8fd6eb477288
hikaru9507 <52337447+hikaru9507@users.noreply.github.com>
2021-05-26 10:58:48 +0900

    first commit

---
+++ actions/blogs_test.go
@@ -0,0 +1,31 @@
+package actions
+
+
+func (as *ActionSuite) Test_BlogsResource_List() {
+  as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_BlogsResource_Show() {
+  as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_BlogsResource_Create() {
+  as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_BlogsResource_Update() {
+  as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_BlogsResource_Destroy() {
+  as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_BlogsResource_New() {
+  as.Fail("Not Implemented!")
+}
+
+func (as *ActionSuite) Test_BlogsResource_Edit() {
+  as.Fail("Not Implemented!")
+}
+
